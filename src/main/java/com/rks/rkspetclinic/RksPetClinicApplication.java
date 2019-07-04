014984c538afce58747f7a83a4e5eb2df3b76cfb
ravendraksingh <ravendraksingh@gmail.com>
2019-07-04 22:50:22 +0530

    Initial Project Add

---
+++ src/main/java/com/rks/rkspetclinic/RksPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.rks.rkspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RksPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RksPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rks/rkspetclinic/RksPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rks.rkspetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RksPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
