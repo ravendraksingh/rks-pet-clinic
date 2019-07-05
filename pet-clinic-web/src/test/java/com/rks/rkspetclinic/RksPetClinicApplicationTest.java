8a40eada95ed0c6aa67ca4e85df7fe8de2be1d6c
ravendraksingh <ravendraksingh@gmail.com>
2019-07-06 02:09:50 +0530

    added tests

---
+++ pet-clinic-web/src/test/java/com/rks/rkspetclinic/RksPetClinicApplicationTest.java
@@ -0,0 +1,17 @@
+package com.rks.rkspetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RksPetClinicApplicationTest {
+
+    @Test
+    public void myTest() {
+
+    }
+
+}
