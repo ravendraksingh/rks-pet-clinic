de0822d1f0870fb43ae739ac74e18d3fbefa3da2
ravendraksingh <ravendraksingh@gmail.com>
2019-07-09 21:33:45 +0530

    Create Vet Speciality Entity and associate with Vet. Closes #28

---
+++ pet-clinic-data/src/main/java/com/rks/rkspetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.rks.rkspetclinic.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
