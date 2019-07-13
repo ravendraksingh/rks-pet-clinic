6ee77d38b3e832a90ff71cd2b6a9183e3d1abc66
ravendraksingh <ravendraksingh@gmail.com>
2019-07-13 20:47:10 +0530

    Convert Vets to JPA entities. Closes #38

--- pet-clinic-data/src/main/java/com/rks/rkspetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/rks/rkspetclinic/model/Speciality.java
@@ -1,16 +1,14 @@
 package com.rks.rkspetclinic.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
